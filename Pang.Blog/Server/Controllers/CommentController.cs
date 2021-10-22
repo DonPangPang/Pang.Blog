402508d0a01d2e1b2659c2d8df69556b73a1e81c
DonPangPang <powertle@outlook.com>
2021-10-22 08:24:44 +0800

    update

--- Pang.Blog/Server/Controllers/CommentController.cs
+++ Pang.Blog/Server/Controllers/CommentController.cs
@@ -47,5 +47,71 @@ namespace Pang.Blog.Server.Controllers
 
             return Success(res);
         }
+
+        /// <summary>
+        /// 获取子评论
+        /// </summary>
+        /// <param name="commentId"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        [HttpGet("{commentId:guid}")]
+        public async Task<ActionResult> GetChildrenComments(Guid commentId)
+        {
+            if (commentId == Guid.Empty) throw new ArgumentNullException(nameof(commentId));
+
+            var comments = await _repertory
+                .GetEntitiesAsync(x => x.ParentId.Equals(commentId));
+
+            var res = _mapper.Map<IEnumerable<CommentDto>>(comments);
+
+            return Success(res);
+        }
+
+        /// <summary>
+        /// 评论
+        /// </summary>
+        /// <param name="comment"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        [HttpPost]
+        public async Task<ActionResult> AddComment([FromBody] CommentDto comment)
+        {
+            if (comment == null) throw new ArgumentNullException(nameof(comment));
+
+            var dto = _mapper.Map<Comment>(comment);
+            dto.Create();
+
+            if (await _repertory.InsertAsync(dto))
+            {
+                return Success("评论成功");
+            }
+
+            return Fail("评论失败");
+        }
+
+        /// <summary>
+        /// 删除评论
+        /// </summary>
+        /// <param name="commentId">删除评论的Id</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        [HttpGet("{commentId:guid}")]
+        public async Task<ActionResult> Delete(Guid commentId)
+        {
+            if (commentId == Guid.Empty) throw new ArgumentNullException(nameof(commentId));
+
+            var comment = new Comment()
+            {
+                Id = commentId,
+                DeleteMark = true
+            };
+
+            if (await _repertory.DeleteAsync(comment))
+            {
+                return Success("删除成功");
+            }
+
+            return Fail("删除失败");
+        }
     }
 }
--- Pang.Blog/Server/Extensions/EntityExtensions.cs
+++ Pang.Blog/Server/Extensions/EntityExtensions.cs
@@ -4,8 +4,17 @@ using Pang.Blog.Server.Entities.Base;
 
 namespace Pang.Blog.Server.Extensions
 {
+    /// <summary>
+    /// 实体扩展
+    /// </summary>
     public static class EntityExtensions
     {
+        /// <summary>
+        /// 将实体集合的Id转为字符串
+        /// </summary>
+        /// <param name="list"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static string ToLineString<T>(this IEnumerable<T> list) where T : EntityBase
         {
             return string.Join(",", list.Select(x => x.Id));
